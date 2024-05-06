565cbc5ad0aeb089db923b85fac291585dbb43d5
sakshidalalsd <139738505+sakshidalalsd@users.noreply.github.com>
2024-05-06 20:30:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-//your JS code here. If required.
+let txt=document.querySelectorAll('.code');
+   let index = 0;
+    function focused(){
+		 if(index < txt.length - 1) {
+        index++;
+        txt[index].focus();
+    }
+	}
+function handleBackspace(e) {
+    if(e.key === 'Backspace' && index > 0) {
+        txt[index].value = '';
+        index--;
+        txt[index].focus();
+    }
+}
+
+txt.forEach((i) => {
+    i.addEventListener('input', focused);
+    i.addEventListener('keydown', handleBackspace);
+});
+  
+
+      
